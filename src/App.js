9e0beca098f42b486386f08062b7123c49378d13
Joanna-Golofit <sunderlajsa@gmail.com>
2021-12-04 20:54:01 +0100

    dodawanie do todolisty - pytanie do Mikolaja

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+
+class ToDoList extends Component {
+  static defaultProps = {
+    title: "Ten tytul jesli props nie byl podany",
+    tasks: ["Zad 6", "Zad 7", "Zad 8"]
+  };
+
+  state = {
+    tasks: this.props.tasks,
+    draft: "",
+  }
+
+  updateDraft = (e) => {
+    this.setState(state => ({ draft: e.target.value }));
+    console.log("draft:", this.state.draft)
+  }
+
+  addToDo = () => {
+    const { tasks, draft } = this.state;
+    // this.setState({ tasks: tasks.push(draft), draft: "" });
+    const list = tasks
+    list.push(draft)
+    // this.setState({tasks: list, draft: ""})
+    // this.setState({ ...this.state, tasks: tasks.push(draft) });
+    // this.setState(state => ({ ...state, tasks: tasks.push(draft) }))
+    this.setState(state => ({ ...state, tasks: list, draft: "" }))
+  }
+  
+  klik = () => console.log("this.state:", this.state , "this.props:", this.props)
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.title}</h2>
+        <div>
+          <p>Zad 1</p>
+        </div>
+        {this.props.tasks.map((task) => (
+          <div key={task}>
+            <p>{task}</p>
+          </div>
+        ))}
+        <input onChange={this.updateDraft} type="text" value={this.state.draft}/>
+        <button onClick={this.addToDo}>Add</button>
+      </div>
+    );
+  }
+}
+// export default ToDoList
 
 function App() {
+  const myTasks = ["Zad 3", "Zad 4", "Zad 5"]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ToDoList title="Moja lista" tasks={myTasks} />
+      <ToDoList />
     </div>
   );
 }
